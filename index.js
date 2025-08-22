8fad8373f56d587afadbdb516d993aeb0449ff4c
Пламен Сидеров <stu2401261015@uni-plovdiv.bg>
2025-08-22 14:24:35 +0300

    Fixing mobile.6

---
+++ index.js
@@ -0,0 +1,16 @@
+const menueBackground = document.getElementById("main_menu_background");
+
+function isMobile() {
+    return window.innerWidth <= 414 || /Android|iPhone|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
+}
+
+function updateMenuWidth() {
+    const width = window.innerWidth;
+    menueBackground.style.width = width + "px";
+}
+
+if (isMobile()) {
+    updateMenuWidth();
+    window.addEventListener("resize", updateMenuWidth);
+    console.log("Mobile mode activated!");
+}
