e935a282b9b7751e7c937f3b50a039fec86d58c0
Пламен Сидеров <stu2401261015@uni-plovdiv.bg>
2025-08-23 09:16:18 +0300

    Fixing the mobile version.8

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const menueBackground = document.getElementById("main_menu_background");
+const menuBackground = document.getElementById("main_menu_background");
 
 function isMobile() {
     return window.innerWidth <= 414 || /Android|iPhone|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
 }
 
 function updateMenuWidth() {
-    const width = window.innerWidth;
-        menuBackground.style.width = window.innerWidth + "px";
-    }
-
-if (isMobile()) {
-    updateMenuWidth();
-    window.addEventListener("resize", updateMenuWidth);
-    console.log("Mobile mode activated!");
+    // Always match element width to window width
+    menuBackground.style.width = window.innerWidth + "px";
+    console.log(menuBackground.width);
 }
-else{
-    console.log("I am on a computer");
+
+// Run once when the page loads
+updateMenuWidth();
+
+// Run whenever the window is resized
+window.addEventListener("resize", updateMenuWidth);
+
+// Debug logging
+if (isMobile()) {
+    console.log("Mobile mode activated!");
+} else {
+    console.log("I am on a computer!!!!");
 }
