29de1d595e97a75a0a4cee05217a6baf3fb75a51
Пламен Сидеров <stu2401261015@uni-plovdiv.bg>
2025-08-23 09:35:11 +0300

    Fixing the mobile version.9

--- name_generator_script.js
+++ name_generator_script.js
@@ -1,3 +1,28 @@
+//
+const menuBackground = document.getElementById("main_menu_background");
+
+function isMobile() {
+    return window.innerWidth <= 414 || /Android|iPhone|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
+}
+
+function updateMenuWidth() {
+    // Always match element width to window width
+    menuBackground.style.width = window.innerWidth + "px";
+    console.log(menuBackground.width);
+}
+
+// Run once when the page loads
+updateMenuWidth();
+
+// Run whenever the window is resized
+window.addEventListener("resize", updateMenuWidth);
+
+// Debug logging
+if (isMobile()) {
+    console.log("Mobile mode activated!");
+} else {
+    console.log("I am on a computer!!!!");
+}
 
 //html elemts being cheked and changed
 const Button = document.getElementById("start_name_search");
